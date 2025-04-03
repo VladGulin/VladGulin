faff6a5eae2c821fd355733f02a756b04cd7428d
VladGulin <gvitaly1603@gmail.com>
2025-04-03 17:45:56 +0400

    This is my project (website)

---
+++ main.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request, redirect
+from json import dump, load
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    with open ('posts.json') as file:
+        dict_file = load(file)
+    return render_template("index.html", posts=dict_file)
+
+@app.route('/create_post', methods=["POST"])
+def func():
+    title = request.form.get('title')
+    text = request.form.get('text')
+    kartinka = request.files.get('kartinka')
+
+    puti_kartinki = "static\\upload\\" + kartinka.filename
+
+    kartinka.save(puti_kartinki)
+
+    with open ('posts.json') as file:
+        dict_file = load(file)
+        new_post = {
+            'text': text,
+            'image': puti_kartinki
+        }
+        dict_file[title] = new_post
+        with open ('posts.json', 'w', encoding='utf-8') as file_2:
+            dump(dict_file, file_2)
+
+
+    return redirect('/')
+
+@app.route('/createPost')
+def get_create_post_template():
+    return render_template("createPost.html")
+
+@app.route('/registration')
+def get_registration_template():
+    return render_template('registration.html')
+
+app.run(debug=True)
